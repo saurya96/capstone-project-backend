ba75dc28b7272ebc7fa61d4c0f8e6d5116ad9b8a
saurya96 <aashikasingh24@navgurukul.org>
2026-02-07 16:33:56 +0530

    Initial backend setup with JSON Server

---
+++ index.js
@@ -0,0 +1,8 @@
+import fs from "fs";
+import path from "path";
+
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), "db.json");
+  const data = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+  res.status(200).json(data);
+}
---
+++ server.js
@@ -0,0 +1,23 @@
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('db.json');
+// const middlewares = jsonServer.defaults({
+//   static: './public'
+// });
+
+// const PORT = process.env.PORT || 5000;
+
+// // Enable CORS for all origins
+// server.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//   next();
+// });
+
+// server.use(middlewares);
+// server.use(router);
+
+// server.listen(PORT, '0.0.0.0', () => {
+//   console.log(`JSON Server is running on port ${PORT}`);
+// });

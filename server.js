dc4038373569e6a9639908adca2be6979aa3c38b
saurya96 <aashikasingh24@navgurukul.org>
2026-02-07 17:04:40 +0530

    Fix CORS configuration for Vercel deployment

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-// const jsonServer = require('json-server');
-// const server = jsonServer.create();
-// const router = jsonServer.router('db.json');
-// const middlewares = jsonServer.defaults({
-//   static: './public'
-// });
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults({
+  static: './public'
+});
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-// // Enable CORS for all origins
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-//   next();
-// });
+// Enable CORS for all origins
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
-// server.use(middlewares);
-// server.use(router);
+server.use(middlewares);
+server.use(router);
 
-// server.listen(PORT, '0.0.0.0', () => {
-//   console.log(`JSON Server is running on port ${PORT}`);
-// });
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
